10ee6d10f06153d63fe2688ec2723b9fb0359673
Tzipota paternak <tz0556776105@gmail.com>
2023-09-19 18:36:32 +0300

    I am tzipi

--- dotNet5783_4452_4387/Stage0/Program4452.cs
+++ dotNet5783_4452_4387/Stage0/Program4452.cs
@@ -5,6 +5,7 @@ namespace Stage0
         static partial void wellcome4452()
         {
             Console.WriteLine("I am also here! I am lea");
+            Console.WriteLine("I am also here! I am tzipi");
         }
     }
 }
