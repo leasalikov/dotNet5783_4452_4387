943974ea967ed621d066dd6f72137e231b14db0d
leasalikov <Hand4c18@mbjcomp.org.il>
2023-10-25 16:45:06 +0300

    7 פרקים ראשונה!!!!!!!!!!!

---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace DO;
+
+/// <summary>
+/// describes the tasks of the enigneersin the company
+/// </summary>
+/// <param name="Description">describes the task</param>
+/// <param name="Nickname">the name that calls the task</param>
+/// <param name="Milestone">?????????????????</param>
+/// <param name="Production">task creation date</param>
+/// <param name="Start">task start date</param>
+/// <param name="EstimatedCompletion">estimated date for completion of the task</param>
+/// <param name="Final">last date for completing the task</param>
+/// <param name="AcualEndNate">actual assignment completion date</param>
+/// <param name="Product">what should come out at the end of the mission</param>
+/// <param name="Remaeks">notes to the engineer</param>
+/// <param name="">identify the engineer assigned to the task</param>
+public record Task
+(
+    //מספר מזהה רץ
+    string Description,
+    string Nickname,
+    bool Milestone,
+    DateTime Production,
+    DateTime? Start,
+    DateTime EstimatedCompletion,
+    DateTime Final,
+    DateTime? AcualEndNate,
+    string Product,
+    string Remaeks,
+    //Engineer
+    //Difficulty
+);
+    
+
---
+++ DalList/DataSource .cs
@@ -0,0 +1,19 @@
+﻿
+namespace Dal;
+
+internal static class DataSource
+{
+
+    internal static class Config
+    {
+        internal const int startTaskId = 1;
+        private static int nextTaskId = startTaskId;
+        internal static int NextTaskId { get => nextTaskId++; }
+        
+    }
+
+
+    internal static List<DO.Task> Tasks { get; } = new();
+
+
+}
