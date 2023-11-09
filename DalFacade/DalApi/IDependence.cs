00c9475cf1efef7c2207a9ac571d9d7795366bd3
thipora <Hand4c25@mbjcomp.org.il>
2023-11-09 20:01:00 +0200

    ff

--- DalFacade/DalApi/IDependence.cs
+++ DalFacade/DalApi/IDependence.cs
@@ -1,11 +1,3 @@
-﻿namespace DalApi;
-using DO;
-public interface IDependence
-{
-    int Create(Dependence item); //Creates new entity object in DAL
-    Dependence? Read(int id); //Reads entity object by its ID 
-    List<Dependence> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Dependence item); //Updates entity object
-    void Delete(int id); //Deletes an object by its Id
-
-}
+﻿using DO;
+namespace DalApi;
+public interface IDependence : ICrud<Dependence> { }
