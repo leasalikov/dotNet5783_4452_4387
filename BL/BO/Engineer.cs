ed3cb90af6e32d15fc337bad21b1933f88eecc3e
leasalikov <Hand4c18@mbjcomp.org.il>
2023-12-27 12:46:12 +0200

    123

--- BL/BO/Engineer.cs
+++ BL/BO/Engineer.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BO
+namespace BO;
+
+public class Engineer
 {
-    internal class Engineer
-    {
-    }
+
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public EngineerLevelEnum EngineerLevel { get; set; }
+    public float PriceOfHour { get; set; }
+
+
+
 }
--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BO
+﻿
+namespace BO;
+public enum EngineerLevelEnum
 {
-    internal class Enums
-    {
-    }
+    Novice, AdvancedBeginner, Competent, Proficient, Expert
+}
+
+public enum Status
+{
+    Unscheduled, Scheduled, OnTrack, InJeopardy
 }
