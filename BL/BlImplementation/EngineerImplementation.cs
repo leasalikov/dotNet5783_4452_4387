0f9ba18847cb0f7454d6e297073aa315c851c16b
thipora <tz0556727951@gmail.com>
2023-12-30 23:06:17 +0200

    התחלת שלב 7

---
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,54 @@
+﻿
+using BlApi;
+using BO;
+
+namespace BlImplementation;
+
+internal class EngineerImplementation : IEngineer
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+
+     public int Create(BO.Engineer boEngineer)
+    {
+        DO.Engineer doEngineer = new DO.Engineer
+        (boEngineer.Id,
+        boEngineer.Name, 
+        boEngineer.Email,
+        (DO.EngineerExperience)boEngineer.Level,
+        boEngineer.Cost);
+        try
+        {
+            int idEng = _dal.Engineer.Create(doEngineer);
+            return idEng;
+        }
+        catch (DO.DalAlreadyExistsException exception)
+        {
+            throw new BO.BlAlreadyExistsException($"An object of type Engineer with ID {boEngineer.Id} already exists", exception);
+        }
+
+    }
+    public void add(Engineer engineer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Engineer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Engineer engineer)
+    {
+        throw new NotImplementedException();
+    }
+}
