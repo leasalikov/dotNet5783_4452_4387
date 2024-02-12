f6dbf5d701443c05edda6c607ba71d85205ed0ab
leasalikov <Hand4c18@mbjcomp.org.il>
2024-02-12 18:57:07 +0200

    פרק 7

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace PL;
 
-internal class EngineersTeam : IEnumerable
+internal class EngineersCollection : IEnumerable
 {
     static readonly IEnumerable<BO.EngineerLevelEnum> s_enums =
         (Enum.GetValues(typeof(BO.EngineerLevelEnum)) as IEnumerable<BO.EngineerLevelEnum>)!;
