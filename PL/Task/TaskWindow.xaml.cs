dc7d36b247b3bbe0ba73957a105f37a040f555e9
thipora <tz0556727951@gmail.com>
2024-02-12 21:26:11 +0200

    start stage 8

--- PL/Task/TaskWindow.xaml.cs
+++ PL/Task/TaskWindow.xaml.cs
@@ -1,27 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace PL.Task
+namespace PL.Task;
+
+/// <summary>
+/// Interaction logic for TaskWindow.xaml
+/// </summary>
+public partial class TaskWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for TaskWindow.xaml
-    /// </summary>
-    public partial class TaskWindow : Window
+    static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+    public TaskWindow(int Id = 0)
     {
-        public TaskWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        CurrentTask = Id == 0 ? s_bl?.Task.Read(Id) : null;
     }
+
+
+    public BO.Task CurrentTask
+    {
+        get { return (BO.Task)GetValue(CurrentTaskProperty); }
+        set { SetValue(CurrentTaskProperty, value); }
+    }
+
+    public static readonly DependencyProperty CurrentTaskProperty =
+        DependencyProperty.Register("CurrentTask", typeof(BO.Task), typeof(TaskWindow), new PropertyMetadata(null));
+
+    private void btnAddUpdate_Click(object sender, SelectionChangedEventArgs e)
+    {
+        //TaskList = TaskToList(s_bl?.Task.ReadAll(Status));
+    }
+
+
+
 }
